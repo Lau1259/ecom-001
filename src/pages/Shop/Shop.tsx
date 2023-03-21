197e751e8e2add89924bc75c3768947cfc19e923
Lautaro Cuevas <lautaro.cuevas.rodas@gmail.com>
2023-03-20 23:06:54 -0300

    started shop page

---
+++ src/pages/Shop/Shop.tsx
@@ -0,0 +1,12 @@
+import { CardContainer } from "../Tests/styles";
+// import { Shop } from "./styles";
+
+interface Props {
+  //Describe Props
+}
+
+const Shop = ({}) => {
+  return <div></div>;
+};
+
+export default Shop;
